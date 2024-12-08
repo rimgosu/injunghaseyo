8a4f265b7883bcc8979c85f3c5570f2b779cff6d
ji-hyup <newnyup@gmail.com>
2024-12-08 15:14:49 +0900

    env: prisma service init

--- src/backend/src/app.module.ts
+++ src/backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersAuthModule } from './users-auth/user-auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UsersAuthModule],
+  imports: [UsersAuthModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
