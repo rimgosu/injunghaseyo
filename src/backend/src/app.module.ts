2a56f54210c397bdce9fd3eb2e93a5306f67edf9
ji-hyup <newnyup@gmail.com>
2024-12-08 22:04:57 +0900

    cannot connect redis.. why?

--- src/backend/src/app.module.ts
+++ src/backend/src/app.module.ts
@@ -3,9 +3,35 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersAuthModule } from './users-auth/user-auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-yet';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [UsersAuthModule, PrismaModule],
+  imports: [
+    UsersAuthModule,
+    PrismaModule,
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+    }),
+    CacheModule.registerAsync({
+      isGlobal: true,
+      inject: [ConfigService],
+      useFactory: async (config) => {
+        const store = await redisStore({
+          ttl: 180 * 1000,
+          socket: {
+            host: config.get('redis.host'),
+            port: config.get('redis.port'),
+          },
+          password: config.get('redis.password'),
+        });
+        return { store };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
