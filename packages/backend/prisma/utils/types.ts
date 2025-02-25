e51205620caeec0ce91635bb33d0b375387faee5
ji-hyup <newnyup@gmail.com>
2025-02-25 22:16:24 +0900

    delete: 더이상 사용 안하는 타입 제거

--- packages/backend/prisma/utils/types.ts
+++ packages/backend/prisma/utils/types.ts
@@ -1,9 +1,3 @@
-import { Prisma } from '@prisma/client';
-
 export const yelloName = '노랑이';
 export const greenName = '초록이';
 export const blueName = '파랑이';
-
-export const inProgressGroupId = 999999;
-
-export type GroupSeedDataInput = Prisma.GroupCreateInput;
