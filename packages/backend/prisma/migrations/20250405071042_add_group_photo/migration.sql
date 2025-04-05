a336994095a0c18c0e59d47e5f7ca02e9afa06ab
rimgosu <newnyup@gmail.com>
2025-04-05 16:13:31 +0900

    feat: 모임 사진 기능 추가

---
+++ packages/backend/prisma/migrations/20250405071042_add_group_photo/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Group" ADD COLUMN     "photo" TEXT NOT NULL DEFAULT 'https://injunghaseyo-dev.s3.ap-northeast-2.amazonaws.com/default/group-default-image.png';
---
+++ packages/backend/prisma/migrations/20250405071259_profile_photo_default_url/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "ProfilePhoto" ALTER COLUMN "url" SET DEFAULT 'https://injunghaseyo-dev.s3.ap-northeast-2.amazonaws.com/default/default-profile.svg';
