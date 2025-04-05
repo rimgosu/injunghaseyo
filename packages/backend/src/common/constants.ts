c5b9ebc2f48e487d0738492906e8db15f8e3465d
rimgosu <newnyup@gmail.com>
2025-04-05 16:17:15 +0900

    fix: s3 폴더 경로 수정

--- packages/backend/src/common/constants.ts
+++ packages/backend/src/common/constants.ts
@@ -1,1 +1,2 @@
-export const BASE_PROFILE_PHOTO_S3_URL = `${process.env.S3_URL}/profile-photo/basic-profile.svg`;
+export const BASE_PROFILE_PHOTO_S3_URL = `${process.env.S3_URL}/default/default-profile.svg`;
+export const BASE_GROUP_PHOTO_S3_URL = `${process.env.S3_URL}/default/group-default-image.png`;
