53560793e8a6c1837c9f6e5b80c09fc8e126a9d5
rimgosu <newnyup@gmail.com>
2025-04-05 14:15:54 +0900

    feat: 오늘의 인증 페이지 - 조금 이쁘게

--- packages/frontend/src/group/pages/GroupTodayPage.tsx
+++ packages/frontend/src/group/pages/GroupTodayPage.tsx
@@ -10,12 +10,12 @@ import {
   CursorArrowRaysIcon,
   MapPinIcon,
 } from '@heroicons/react/24/outline';
+import { formatMinutesToTime } from '../utils/utils';
 
 export const GroupTodayPage = () => {
   const { groupId } = useParams();
   const { getToday } = useGroups();
   const { todayGroup, setTodayGroup } = useTodayGroupStore();
-  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchTodayGroup = async () => {
@@ -35,35 +35,48 @@ export const GroupTodayPage = () => {
           <h2 className="text-2xl font-bold">{todayGroup?.title}</h2>
           <p className="text-gray-500">{todayGroup?.description}</p>
         </div>
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-2">
           <h3 className="text-xl">오늘의 인증</h3>
-          {todayGroup?.proofs.map((proof, index) => {
-            let icon = null;
-            if (
-              proof.proofMethod.type === ProofMethodElemTypeEnum.UPLOAD_PHOTO
-            ) {
-              icon = <CameraIcon />;
-            }
-            if (
-              proof.proofMethod.type === ProofMethodElemTypeEnum.CLICK_BUTTON
-            ) {
-              icon = <CursorArrowRaysIcon />;
-            }
-            if (
-              proof.proofMethod.type === ProofMethodElemTypeEnum.CHECK_LOCATION
-            ) {
-              icon = <MapPinIcon />;
-            }
-            return (
-              <div key={index} className="flex flex-col gap-2">
-                <div className="flex text-gray-600 gap-1">
-                  <p>인증 {index + 1}.</p>
-                  <p>{proof.proofMethod.contents}</p>
+          <div className="flex flex-col gap-4">
+            {todayGroup?.proofs.map((proof, index) => {
+              let icon = null;
+              if (
+                proof.proofMethod.type === ProofMethodElemTypeEnum.UPLOAD_PHOTO
+              ) {
+                icon = <CameraIcon strokeWidth={1} className="w-16" />;
+              }
+              if (
+                proof.proofMethod.type === ProofMethodElemTypeEnum.CLICK_BUTTON
+              ) {
+                icon = <CursorArrowRaysIcon strokeWidth={1} className="w-16" />;
+              }
+              if (
+                proof.proofMethod.type ===
+                ProofMethodElemTypeEnum.CHECK_LOCATION
+              ) {
+                icon = <MapPinIcon strokeWidth={1} className="w-16" />;
+              }
+              return (
+                <div
+                  key={index}
+                  className="flex gap-2 justify-between p-4 border border-gray-300 rounded-2xl"
+                >
+                  <div className="flex text-gray-600 gap-1 justify-center flex-col">
+                    <div className="flex gap-1">
+                      <p>인증 {index + 1}.</p>
+                      <p>{proof.proofMethod.contents}</p>
+                    </div>
+                    <p>
+                      인증 가능 시간:{' '}
+                      {formatMinutesToTime(proof.proofMethod.fromMin)} -{' '}
+                      {formatMinutesToTime(proof.proofMethod.toMin)}
+                    </p>
+                  </div>
+                  <div className="flex justify-center">{icon}</div>
                 </div>
-                <div className="w-36 h-36 text-center">{icon}</div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
       </div>
     </BaseLayout>
