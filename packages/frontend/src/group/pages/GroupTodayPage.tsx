cd9f3386795d019109513ae391f803c62cfaa8fd
rimgosu <newnyup@gmail.com>
2025-04-05 13:55:52 +0900

    feat: 오늘의 인증 - 인증 아이콘 추가

--- packages/frontend/src/group/pages/GroupTodayPage.tsx
+++ packages/frontend/src/group/pages/GroupTodayPage.tsx
@@ -4,6 +4,12 @@ import { useTodayGroupStore } from '../stores/useTodayGroupStore';
 import { useEffect } from 'react';
 import { BaseLayout } from '../../common/BaseLayout';
 import { TodayGroupTopNavBar } from '../components/TodayGroupTopNavBar';
+import { ProofMethodElemTypeEnum } from '@rimgosu/libs';
+import {
+  CameraIcon,
+  CursorArrowRaysIcon,
+  MapPinIcon,
+} from '@heroicons/react/24/outline';
 
 export const GroupTodayPage = () => {
   const { groupId } = useParams();
@@ -29,16 +35,35 @@ export const GroupTodayPage = () => {
           <h2 className="text-2xl font-bold">{todayGroup?.title}</h2>
           <p className="text-gray-500">{todayGroup?.description}</p>
         </div>
-        <div className="flex flex-col gap-2">
-          <h3 className="text-xl text-center">오늘의 인증</h3>
-          {todayGroup?.proofs.map((proof) => (
-            // <div key={proof.}>
-            //   <img src={proof.imageUrl} alt={proof.title} />
-            //   <p>{proof.title}</p>
-            //   <p>{proof.description}</p>
-            // </div>
-            <div>1</div>
-          ))}
+        <div className="flex flex-col gap-4">
+          <h3 className="text-xl">오늘의 인증</h3>
+          {todayGroup?.proofs.map((proof, index) => {
+            let icon = null;
+            if (
+              proof.proofMethod.type === ProofMethodElemTypeEnum.UPLOAD_PHOTO
+            ) {
+              icon = <CameraIcon />;
+            }
+            if (
+              proof.proofMethod.type === ProofMethodElemTypeEnum.CLICK_BUTTON
+            ) {
+              icon = <CursorArrowRaysIcon />;
+            }
+            if (
+              proof.proofMethod.type === ProofMethodElemTypeEnum.CHECK_LOCATION
+            ) {
+              icon = <MapPinIcon />;
+            }
+            return (
+              <div key={index} className="flex flex-col gap-2">
+                <div className="flex text-gray-600 gap-1">
+                  <p>인증 {index + 1}.</p>
+                  <p>{proof.proofMethod.contents}</p>
+                </div>
+                <div className="w-36 h-36 text-center">{icon}</div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </BaseLayout>
