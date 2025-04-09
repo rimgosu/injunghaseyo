a1dca32f6f59271a1f1d886e19786e02c03e2916
ji-hyup <newnyup@gmail.com>
2025-04-09 23:30:56 +0900

    feat: 참여 중, 예약 중, 완료함 상태 표시

--- packages/frontend/src/group/components/GroupCard.tsx
+++ packages/frontend/src/group/components/GroupCard.tsx
@@ -1,18 +1,51 @@
-import { GroupElem } from '@rimgosu/libs';
+import { GroupElem, GroupElemJoinStatusEnum } from '@rimgosu/libs';
 
 interface GroupCardProps {
   group: GroupElem;
   onClick: () => void;
 }
 
+const joinStatus2Text = (joinStatus: GroupElemJoinStatusEnum) => {
+  switch (joinStatus) {
+    case GroupElemJoinStatusEnum.RESERVED:
+      return '참여 예정';
+    case GroupElemJoinStatusEnum.IN_PROGRESS:
+      return '참여 중';
+    case GroupElemJoinStatusEnum.COMPLETED:
+      return '참여 완료';
+    case GroupElemJoinStatusEnum.NOT_JOINABLE:
+    case GroupElemJoinStatusEnum.NOT_JOINED:
+      return null;
+  }
+};
+
+const joinStatus2Color = (joinStatus: GroupElemJoinStatusEnum) => {
+  switch (joinStatus) {
+    case GroupElemJoinStatusEnum.RESERVED:
+      return 'bg-green-300';
+    case GroupElemJoinStatusEnum.IN_PROGRESS:
+      return 'bg-blue-300';
+    case GroupElemJoinStatusEnum.COMPLETED:
+      return 'bg-yellow-300';
+    case GroupElemJoinStatusEnum.NOT_JOINABLE:
+    case GroupElemJoinStatusEnum.NOT_JOINED:
+      return null;
+  }
+};
+
 export const GroupCard = ({ group, onClick }: GroupCardProps) => {
   return (
     <div className="flex rounded-2xl cursor-pointer" onClick={onClick}>
-      <div className="flex-2 justify-center items-center pr-4">
+      <div className="flex-2 justify-center items-center mr-4 relative">
+        <div
+          className={`absolute bottom-0 w-full p-1 rounded-b-3xl text-center shadow-sm ${joinStatus2Color(group.joinStatus)}`}
+        >
+          {joinStatus2Text(group.joinStatus)}
+        </div>
         <img
           src={group.groupPhoto}
           alt="group photo"
-          className="w-44 rounded-lg"
+          className="w-44 rounded-3xl"
         />
       </div>
       <div className="flex-1 flex-col gap-1 flex justify-center">
