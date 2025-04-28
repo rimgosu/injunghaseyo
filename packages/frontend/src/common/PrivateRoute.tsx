6f1fbae7956a31b439145d9c917f033b73a5d789
ji-hyup <newnyup@gmail.com>
2025-04-28 22:04:55 +0900

    fix: private route 제대로 동작 안하는 문제 해결

--- packages/frontend/src/common/PrivateRoute.tsx
+++ packages/frontend/src/common/PrivateRoute.tsx
@@ -1,13 +1,25 @@
 import { Navigate } from 'react-router-dom';
+import { useAuth } from '../auth/hooks/useAuth';
+import { useEffect, useState } from 'react';
 
 interface PrivateRouteProps {
   children: React.ReactNode;
 }
 
 export const PrivateRoute = ({ children }: PrivateRouteProps) => {
-  const token = localStorage.getItem('accessToken');
+  const { checkSignIn } = useAuth();
+  const [isSignedIn, setIsSignedIn] = useState(false);
 
-  if (!token) {
+  useEffect(() => {
+    const check = async () => {
+      const res = await checkSignIn();
+      res.data && setIsSignedIn(true);
+      res.error && setIsSignedIn(false);
+    };
+    check();
+  }, [checkSignIn]);
+
+  if (!isSignedIn) {
     return <Navigate to="/auth/login" replace />;
   }
 
