c336a8b4b7f5c173f164c2f0bbdc442b84e6407f
jihyup-byun <120752098+rimgosu@users.noreply.github.com>
2025-05-11 13:49:11 +0900

    fix: 답글의 답글 시 디자인 이상한거 수정

--- packages/frontend/src/proof/components/comment-core/CommentInputBox.tsx
+++ packages/frontend/src/proof/components/comment-core/CommentInputBox.tsx
@@ -52,10 +52,6 @@ export const CommentInputBox = ({
   }, [contents]);
 
   const handleCreateComment = async () => {
-    console.log('mode:', mode);
-    console.log('parentCommentId:', parentCommentId);
-    console.log('commentId:', commentId);
-
     const res = await createComment(
       {
         proofId: Number(proofId),
@@ -108,7 +104,7 @@ export const CommentInputBox = ({
     >
       <img
         src={checkSignInRes.profilePhoto}
-        className={`w-12 h-12 rounded-full border border-gray-400 ${mode === 'reply' && 'w-9 h-9'}`}
+        className={`w-12 h-12 rounded-full border border-gray-400 ${['reply', 'reply-to-reply'].includes(mode) && 'w-9 h-9'}`}
       />
       <div className="flex-1 flex flex-col gap-2">
         <textarea
@@ -131,10 +127,10 @@ export const CommentInputBox = ({
         />
         {(focused || contents.length > 0) && (
           <div
-            className={`flex justify-end gap-2 ${mode === 'reply' && 'text-md'} ${mode === 'comment' && 'text-lg'}`}
+            className={`flex justify-end gap-2 ${['reply', 'reply-to-reply'].includes(mode) && 'text-md'} ${mode === 'comment' && 'text-lg'}`}
           >
             <p
-              className={`text-gray-500 hover:bg-gray-100 rounded-3xl cursor-pointer ${mode === 'reply' && 'p-1 px-3'} ${mode === 'comment' && 'p-2 px-4'}`}
+              className={`text-gray-500 hover:bg-gray-100 rounded-3xl cursor-pointer ${['reply', 'reply-to-reply'].includes(mode) && 'p-1 px-3'} ${mode === 'comment' && 'p-2 px-4'}`}
               onClick={() => {
                 setFocused(false);
                 setContents('');
@@ -144,7 +140,7 @@ export const CommentInputBox = ({
               취소
             </p>
             <p
-              className={`text-gray-500 bg-gray-300 rounded-3xl cursor-pointer ${contents.length > 0 && 'bg-green-400 text-white font-bold'} ${mode === 'reply' && 'p-1 px-3'} ${mode === 'comment' && 'p-2 px-4'}`}
+              className={`text-gray-500 bg-gray-300 rounded-3xl cursor-pointer ${contents.length > 0 && 'bg-green-400 text-white font-bold'} ${['reply', 'reply-to-reply'].includes(mode) && 'p-1 px-3'} ${mode === 'comment' && 'p-2 px-4'}`}
               onClick={handleCreateComment}
             >
               댓글
