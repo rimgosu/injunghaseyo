849851b7689856e5f10f724a13788200a4516848
jihyup-byun <120752098+rimgosu@users.noreply.github.com>
2025-05-17 13:05:31 +0900

    fix: 댓글 수정, 삭제 z-index 조정

--- packages/frontend/src/proof/components/comment-core/CommentElement.tsx
+++ packages/frontend/src/proof/components/comment-core/CommentElement.tsx
@@ -220,7 +220,7 @@ export const CommentElement = ({
               }}
             />
             {isEllipsisOpen && (
-              <div className="absolute right-0 mt-1 flex flex-col gap-2 rounded-2xl bg-white p-2 drop-shadow-lg">
+              <div className="absolute right-0 z-10 mt-1 flex flex-col gap-2 rounded-2xl bg-white p-2 drop-shadow-lg">
                 <div className="flex cursor-pointer items-center justify-center gap-1 rounded-2xl px-4 py-2 hover:bg-gray-100">
                   <PencilIcon className="h-5 w-5 text-gray-500" />
                   <p className="cursor-pointer whitespace-nowrap rounded-2xl px-2 py-1">
