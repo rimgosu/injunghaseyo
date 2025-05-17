414e4a38aeed1ad9f03b848666b4cfaed52d565a
jihyup-byun <120752098+rimgosu@users.noreply.github.com>
2025-05-17 18:39:06 +0900

    inProgress: 그룹 수정 페이지 작성..

--- packages/frontend/src/group/pages/GroupEditPage.tsx
+++ packages/frontend/src/group/pages/GroupEditPage.tsx
@@ -1,15 +1,29 @@
+import { useParams } from 'react-router-dom';
 import { BaseLayout } from '../../common/BaseLayout';
 import { Input } from '../../common/components/Input';
 import { XButton } from '../../common/components/XButton';
+import { useGroupStore } from '../stores/useGroupStore';
 
 export const GroupEditPage = () => {
+  const { groupId } = useParams<{ groupId: string }>();
+  const { groupData } = useGroupStore(Number(groupId));
+
   return (
     <BaseLayout isMainLogo rightElement={<XButton />}>
       <Input
         label="그룹 제목"
         type="text"
-        value="사랑하는 사람들과 함께 하는 그룹"
+        value={groupData?.title || ''}
         name="title"
+        onChange={() => {
+          console.log('change');
+        }}
+      />
+      <Input
+        label="그룹 설명"
+        type="text"
+        value={groupData?.description || ''}
+        name="description"
         onChange={() => {
           console.log('change');
         }}
